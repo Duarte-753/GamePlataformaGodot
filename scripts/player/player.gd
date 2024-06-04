18df3eef27a2b8d959a987de3327d17ad5c149bf
Duarte-753 <julioduartebatista753@gmail.com>
2024-06-04 00:31:41 -0300

    Precisa verificar script Player

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,16 +2,35 @@ extends CharacterBody2D
 class_name Player
 
 @onready var player_sprite: Sprite2D = get_node("Texture")
-
 @export var speed: int
+@export var jump_speed: int
+@export var player_gravity: int
+var jump_count: int = 0
+var landing: bool = false
+
 
 func _physics_process(delta):
 	horizontal_movement_env()
+	vertical_movement_env()
+	
+	gravity(delta)
 	move_and_slide()
 	player_sprite.animate(velocity)
 	
 func horizontal_movement_env() -> void:
 	#personagem irá andar para esquerda e direita
-	var input_direction =Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
+	var input_direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
 	velocity.x = input_direction * speed
 	
+func vertical_movement_env() -> void:
+	if is_on_floor():
+		jump_count = 0
+	if Input.is_action_just_pressed("ui_select") and jump_count < 2:
+		jump_count += 1
+		velocity.y = jump_speed
+	
+
+func gravity(delta) -> void:
+	velocity.y += delta * player_gravity
+	if velocity.y >= player_gravity:
+		velocity.y = player_gravity
--- scripts/player/player_texture.gd
+++ scripts/player/player_texture.gd
@@ -2,18 +2,33 @@ extends Sprite2D
 class_name PlayerTexture
 
 @onready var animation : AnimationPlayer = get_node("../Animation")
+@onready var player : CharacterBody2D = get_node(Player)
 
 
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
-	horizontal_behavior(direction)
+	
+	if direction.y !=0:
+		vertical_behavior(direction)
+	elif player.landing == true:
+		animation.play("landing")
+	else:
+		horizontal_behavior(direction)
+	
 
 func  verify_position(direction: Vector2) -> void:
 	if direction.x > 0:
 		flip_h = false
 	elif direction.x < 0:
 		flip_h = true
-	
+
+func vertical_behavior(direction: Vector2) -> void:
+	if direction.y > 0:
+		player.landing = true
+		animation.play("fall")
+	elif direction.y < 0:
+		animation.play("jump")
+
 func horizontal_behavior(direction: Vector2) -> void:
 	if direction.x != 0:
 		#play na animação run
