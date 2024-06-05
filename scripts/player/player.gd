499be0f9eaa2befd6e1860f7004518a1f275b031
Duarte-753 <julioduartebatista753@gmail.com>
2024-06-05 00:18:59 -0300

    Continuar aula 52

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 class_name Player
 
-@onready var player_sprite: Sprite2D = get_node("Texture")#acesso as minhas texturas
+@onready var player_sprite: Sprite2D = get_node("Texture")#acesso as minhas texturas filho de player
+@onready var wall_ray: RayCast2D = get_node("WallRay")#acesso ao filho Wallray sobre slide da parede
 @export var speed: int #velocidade
 @export var jump_speed: int #velocidade de pulo
 @export var player_gravity: int #gravidade do player
@@ -18,6 +19,7 @@ func _physics_process(delta): #processo de físicas
 	horizontal_movement_env() #movimento horizontal
 	vertical_movement_env()#movimento vertical
 	action_env()#ações de defender, etc..
+	
 	gravity(delta)#gravidade
 	move_and_slide()
 	player_sprite.animate(velocity)#animação
@@ -25,12 +27,15 @@ func _physics_process(delta): #processo de físicas
 func horizontal_movement_env() -> void:
 	#personagem irá andar para esquerda e direita
 	var input_direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
+	if can_track_input == false or attacking:
+		velocity.x = 0
+		return
 	velocity.x = input_direction * speed
 	
 func vertical_movement_env() -> void:
 	if is_on_floor():#se ele tiver no chão zerar os count pulos
 		jump_count = 0
-	if Input.is_action_just_pressed("ui_select") and jump_count < 2:# se precionar o SPACE ira pular no máximo 2 pulos
+	if Input.is_action_just_pressed("ui_select") and jump_count < 2 and can_track_input and not attacking:# se precionar o SPACE ira pular no máximo 2 pulos
 		jump_count += 1 #conta os pulos
 		velocity.y = jump_speed#faz os pulos
 	
@@ -40,11 +45,30 @@ func action_env() -> void:
 	defense()
 	
 func attack() -> void:
-	pass
+	var attack_condition: bool = not attacking and not crouching and not defending
+	if Input.is_action_just_pressed("attack") and attack_condition and is_on_floor():
+		attacking = true
+		player_sprite.normal_attack = true
+	
 func crouch() -> void:
-	pass
+	if Input.is_action_pressed("crouch") and is_on_floor() and not defending:
+		crouching = true
+		can_track_input = false
+	elif  not defending:
+		crouching = false
+		can_track_input = true
+		player_sprite.crouching_off = true
+		
 func defense() -> void:
-	pass
+	if Input.is_action_pressed("defense") and is_on_floor() and not crouching:
+		defending = true
+		can_track_input = false
+	elif  not crouching:
+		defending = false
+		can_track_input = true
+		player_sprite.shield_off = true
+		
+	
 func gravity(delta) -> void:
 	velocity.y += delta * player_gravity
 	if velocity.y >= player_gravity:
