8e3273f578e8ba943dac2aa631d2f63efa292dd6
Duarte-753 <julioduartebatista753@gmail.com>
2024-06-09 14:01:43 -0300

    Faznedo inimigos scripts

---
+++ scripts/enemy/enemy_texture.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+class_name EnemyTexture
+
+@onready var animation : AnimationPlayer = get_node("../Animation")
+@onready var enemy : CharacterBody2D = get_node("../")
+@onready var attack_area_collision : CollisionObject2D = get_node("../AttackArea")
+
+func animate(_velocity) -> void:
+	pass
+
+
+func _on_animation_finished(_anim_name: String) -> void:
+	pass # Replace with function body.
