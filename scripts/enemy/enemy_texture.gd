2bb1c4f7f3171ce9d334834c89a5fc46215c7121
Duarte-753 <julioduartebatista753@gmail.com>
2024-06-09 17:38:58 -0300

    Terminando inimigos baleia

--- scripts/enemy/enemy_texture.gd
+++ scripts/enemy/enemy_texture.gd
@@ -3,7 +3,7 @@ class_name EnemyTexture
 
 @onready var animation : AnimationPlayer = get_node("../Animation")
 @onready var enemy : CharacterBody2D = get_node("../")
-@onready var attack_area_collision : CollisionObject2D = get_node("../AttackArea")
+@onready var attack_area_collision : CollisionObject2D = get_node("../EnemyAttackArea")
 
 func animate(_velocity) -> void:
 	pass
